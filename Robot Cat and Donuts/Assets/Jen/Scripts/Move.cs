b6d1af4a90277c672b39be02fd58cfb4e01eeed9
Jake <dev@intrepidshape.com>
2022-06-23 19:10:02 +0800

    Rotation script

--- Robot Cat and Donuts/Assets/Jen/Scripts/Move.cs
+++ Robot Cat and Donuts/Assets/Jen/Scripts/Move.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.InputManager;
 using UnityEngine;
 
 public class Move : MonoBehaviour
--- Robot Cat and Donuts/Assets/Rotation.cs
+++ Robot Cat and Donuts/Assets/Rotation.cs
@@ -4,18 +4,29 @@ using UnityEngine;
 
 public class Rotation : MonoBehaviour
 {
+    public float speed = 4f;
+    Im_Master _inputs;
+
+    private void Awake()
+    {
+        _inputs = new Im_Master();
+    }
+
+    private void OnEnable()
+    {
+        _inputs.main.Enable();
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Move"))
-        {
-            
-        }
+        float x = _inputs.main.main_button.ReadValue<float>();
+        transform.position += new Vector3(x, 0, 0) * speed * Time.deltaTime;
     }
 }
