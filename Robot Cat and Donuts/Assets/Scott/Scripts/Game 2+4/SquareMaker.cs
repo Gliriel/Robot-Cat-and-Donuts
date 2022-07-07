bd990df2811ace4f2d0f6f0c4c9f9b38fa5b383a
Scott <1026018@student.sae.edu.au>
2022-07-07 18:22:30 +0800

    push for Scott

---
+++ Robot Cat and Donuts/Assets/Scott/Scripts/Game 2+4/SquareMaker.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareMaker : MonoBehaviour
+{
+    public GameObject carble;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var randNum = Random.Range(8, 20); // this will return a number between 5 and 15 
+
+        for (var i = 0; i < randNum; i++)
+        {
+            GameObject m = Instantiate(carble, new Vector3(Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f), -0.05f), Quaternion.identity);
+            var randScale = Random.Range(0.1f, 0.25f);
+            m.transform.localScale = new Vector2(randScale, randScale);
+
+        }
+    }
+}
