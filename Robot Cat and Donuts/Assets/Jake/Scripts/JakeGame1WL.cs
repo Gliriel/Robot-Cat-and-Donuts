503799e6b1a8cf5b77c8332b14572041acc713b3
Intrepidshape <intrepidshape@gmail.com>
2022-08-18 15:01:38 +0800

    Auto stash before checking out "HEAD"

--- Robot Cat and Donuts/Assets/Jake/Scripts/JakeGame1WL.cs
+++ Robot Cat and Donuts/Assets/Jake/Scripts/JakeGame1WL.cs
@@ -7,10 +7,11 @@ public class JakeGame1WL : WinLose
     // Start is called before the first frame update
     void Start()
     {
-        GameObject go = GameObject.Find("Battery");
-        base.battery = go.GetComponent<Battery>();
         base.SetWin(true);
         Time.timeScale = 1;
+        GameObject go = GameObject.Find("Battery");
+        base.battery = go.GetComponent<Battery>();
+        
     }
 
     // Update is called once per frame
